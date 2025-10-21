b0e6ff8eb13c3d09f1084a38a4520488c044f594
Guilherme Ledes <ledes.guilherme@gmail.com>
2025-10-21 18:46:34 +0100

    update gh-actions

---
+++ scripts/prepare-docs.mjs
@@ -0,0 +1,22 @@
+import fs from "fs";
+import path from "path";
+
+const outDir = path.join(process.cwd(), "docs");
+fs.mkdirSync(outDir, { recursive: true });
+
+const html = `<!doctype html>
+<meta charset="utf-8">
+<title>fundamentus-proxy</title>
+<h1>fundamentus-proxy</h1>
+<ul>
+  <li><a href="./resultado.html">resultado.html</a></li>
+  <li><a href="./resultado.csv">resultado.csv</a></li>
+</ul>
+`;
+
+fs.writeFileSync(path.join(outDir, "index.html"), html, "utf8");
+
+// desabilita Jekyll no GitHub Pages
+fs.writeFileSync(path.join(outDir, ".nojekyll"), "", "utf8");
+
+console.log("[prepare:docs] ok");
